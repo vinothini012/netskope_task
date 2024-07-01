ed9d9c6b9888b26d83210de4db263c11f9f40657
Vinothini Kumarraja <vinothini211091@gmail.com>
2024-07-02 00:01:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,19 @@ import requests
 from markupsafe import escape
 
 app = Flask(__name__)
+
+@app.route('/fetch')
+def fetch():
+    url = request.args.get('url')
+    if url:
+        response = requests.get(url)
+        return response.content
+    return 'Please provide a URL parameter.'
+
+@app.route('/greet')
+def greet():
+    name = request.args.get('name', 'Guest')
+    return f'<h1>Hello, {name}
+    return f'<h1>Hello, vino
+if __name__ == '__main__':
+    app.run(debug=True)
